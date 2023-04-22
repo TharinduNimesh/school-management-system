ad8e4ccafb65bd86e1c4ebe2d929fdc287ef9121
TharinduNimesh <tharindunimesh.abc@gmail.com>
2023-04-22 15:34:43 +0530

    Revert "Revert "admin register backend""
    This reverts commit cddc1312d89f2b57ba9d6ca4211b8b062bc476ba.

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -3,29 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use MongoDB\BSON\ObjectId;
 use \App\Models\Student;
-use Illuminate\Support\Facades\DB;
 
 class ClassController extends Controller
 {
     public function search_register(Request $request) {
 
-        DB::collection('students')->raw(function ($collection) {
-                return $collection->aggregate([
-                    [
-                        '$match' => [
-                            'age' => ['$gt' => 18],
-                            'gender' => 'female'
-                        ]
-                    ],
-                    [
-                        '$group' => [
-                            '_id' => '$class',
-                            'count' => ['$sum' => 1]
-                        ]
-                    ]
-                ]);
-        });
+        // get student data from student collection for given grade, year and class
+        $students = Student::select(['index_number', 'initial_name'])
+                            ->where('enrollments' , 'elemMatch', [
+                                'year' => $request->year,
+                                'grade' => $request->grade,
+                                'class' => $request->class
+                            ])
+                            ->get();
 
+        // check query return any values
+        if(!$students->isEmpty()) {
+            return $students;
+        } 
+            // if no any values for given data this will return
+        else {
+            $response = new \stdClass();
+            $response->classStatus = 'newClass';
+            return json_encode($response);
+        }
+    }
+
+    public function remove_student(Request $request) {
+        // Removal of year details provided from student enrollment details 
+        $update = Student::where('_id' , $request->studentId)
+                        ->pull('enrollments', [
+                            'year' => $request->year
+                        ]);
+
+        return 'success';
     }
 }
