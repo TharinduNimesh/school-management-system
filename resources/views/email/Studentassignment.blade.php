786bef122a3e59a1e34e81197a90cc174938fc6a
Dilini Tharaka <130860514+dilini-tharaka@users.noreply.github.com>
2023-04-29 22:52:51 +0530

    email bug fixed

--- resources/views/email/Studentassignment.blade.php
+++ resources/views/email/Studentassignment.blade.php
@@ -1,22 +1,23 @@
 <x-mail::message>
-    Dear {{ $data["student_name"] }},
 
-    I am writing to inform you about the homework assigned to you between start date {{$data["start_date"]}} and end date {{$data["end_date"]}}. Our school uses a management system that allows us to keep track of assignments and grades for each student, and I wanted to let you know that you have been assigned the following homework:
+Dear {{ $data["student_name"] }},
 
-        @php
-            $url = $data["url"];
-        @endphp
-        <x-mail::button :url="$url">
-        Visit Now
-        </x-mail::button>
+I am writing to inform you about the homework assigned to you between start date {{$data["start_date"]}} and end date {{$data["end_date"]}}. Our school uses a management system that allows us to keep track of assignments and grades for each student, and I wanted to let you know that you have been assigned the following homework:
 
-    Please ensure that you complete the assigned homework by the due date indicated in the system. Completing homework on time is an important part of your academic success and helps you to stay on track with your learning goals.
+@php
+$url = $data["url"];
+@endphp
+<x-mail::button :url="$url">
+Visit Now
+</x-mail::button>
 
-    If you need any assistance in completing the homework or have any questions about the assignment, please do not hesitate to reach out to your teacher for support.
+Please ensure that you complete the assigned homework by the due date indicated in the system. Completing homework on time is an important part of your academic success and helps you to stay on track with your learning goals.
 
-    If you have any questions or concerns regarding your homework or the school's policies regarding assignments, please feel free to contact me or the school management team. We are always happy to help and provide any information you may need.
+If you need any assistance in completing the homework or have any questions about the assignment, please do not hesitate to reach out to your teacher for support.
 
-    Thank you for your cooperation in ensuring that you stay on track with your academic progress.
+If you have any questions or concerns regarding your homework or the school's policies regarding assignments, please feel free to contact me or the school management team. We are always happy to help and provide any information you may need.
+
+Thank you for your cooperation in ensuring that you stay on track with your academic progress.
         
 Thanks,<br>
 {{ env('SCHOOL_NAME') }} Management Team
