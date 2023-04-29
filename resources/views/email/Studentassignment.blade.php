c69843b6e57c33c5e6ae2025f1103659fc2a6eda
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-04-29 22:19:32 +0530

    New Assignment

--- resources/views/email/Studentassignment.blade.php
+++ resources/views/email/Studentassignment.blade.php
@@ -1,15 +1,17 @@
 <x-mail::message>
-    Dear {{ $data["guardian_Name"] }},
+    Dear {{ $data["student_name"] }},
 
-    I am writing to inform you about your child's assigned homework for today, {{ $data["date"] }}. Our school uses a management system that allows us to keep track of assignments and grades for each student, and I wanted to let you know that your child has been assigned the following homework:
-    
-    {{$data["url"]  }}
+    I am writing to inform you about the homework assigned to you between start date{{$date["date"]}} and end date{{$date["date"]}}. Our school uses a management system that allows us to keep track of assignments and grades for each student, and I wanted to let you know that you have been assigned the following homework:
 
-    Please ensure that your child completes the assigned homework by the due date indicated in the system. Completing homework on time is an important part of your child's academic success and helps them to stay on track with their learning goals.
-    
-    If your child needs any assistance in completing the homework or has any questions about the assignment, please encourage them to reach out to their teacher for support.
-    
-    Thank you for your cooperation in ensuring that your child stays on track with their academic progress.
+    {{["url"]}}
+
+    Please ensure that you complete the assigned homework by the due date indicated in the system. Completing homework on time is an important part of your academic success and helps you to stay on track with your learning goals.
+
+    If you need any assistance in completing the homework or have any questions about the assignment, please do not hesitate to reach out to your teacher for support.
+
+    If you have any questions or concerns regarding your homework or the school's policies regarding assignments, please feel free to contact me or the school management team. We are always happy to help and provide any information you may need.
+
+    Thank you for your cooperation in ensuring that you stay on track with your academic progress.
         
 Thanks,<br>
 {{ env('SCHOOL_NAME') }} Management Team
