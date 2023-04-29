45c8e73c663a283b8a6f61428e6714e37e36078f
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-04-29 20:06:50 +0530

    update

--- resources/views/sport/dashboard.blade.php
+++ resources/views/sport/dashboard.blade.php
@@ -27,7 +27,7 @@
             <!-- Sale & Revenue Start -->
             <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
-                    <div class="col-sm-6 col-xl-4">
+                    <div class="col-sm-6 col-xl-6">
                         <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
                             <i class="fa fa-chart-line fa-3x text-primary"></i>
                             <div class="ms-3">
@@ -37,7 +37,7 @@
                         </div>
                     </div>
 
-                    <div class="col-sm-6 col-xl-4">
+                    <div class="col-sm-6 col-xl-6">
                         <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
                             <i class="fa fa-chart-area fa-3x text-primary"></i>
                             <div class="ms-3">
