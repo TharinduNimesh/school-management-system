2151554d822bd8e2b6a2db68a223b826b039ffcd
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-04-29 18:08:55 +0530

    Attended
    Attended

---
+++ resources/views/email/Absentstudent.blade.php
@@ -0,0 +1,14 @@
+<x-mail::message>
+    Dear {{ $data["guardian_Name"] }},
+
+    I am writing to inform you that your child, {{ $data["student_name"] }}, was absent from school today, {{ $data["date"] }}. Our school uses a management system that allows us to keep track of attendance records for each student, and I wanted to let you know that your child was not present for all scheduled classes today.
+
+    If your child was absent due to illness or other reasons, please provide us with a written excuse within three days of their return to school. We require written excuses for all absences, and failure to provide one may result in an unexcused absence being recorded.
+
+    If you have any questions or concerns regarding your child's attendance record or the school's policies regarding absences, please contact to the school management team. We are always happy to help and provide any information you may need.
+
+    Thank you for your cooperation in ensuring that your child attends school regularly and is able to benefit fully from their education.
+
+    Thanks,<br>
+    {{ env('SCHOOL_NAME') }} Management Team
+</x-mail::message>
---
+++ resources/views/email/Presentstudent.blade.php
@@ -0,0 +1,14 @@
+<x-mail::message>
+    Dear {{ $data["guardian_Name"] }},
+
+    I am writing to inform you about your child's attendance for today, {{ $data["date"] }}. As you may know, our school uses a management system that allows us to keep track of attendance records for each student.
+    
+    I am pleased to report that your child, {{ $data["student_name"] }}, was present for all scheduled classes today. 
+    
+    If you have any questions or concerns regarding your child's attendance record, please contact to the school management team. We are always happy to help and provide any information you may need.
+    
+    Thank you for your cooperation in ensuring that your child attends school regularly and is able to benefit fully from their education.
+
+    Thanks,<br>
+    {{ env('SCHOOL_NAME') }} Management Team
+</x-mail::message>
