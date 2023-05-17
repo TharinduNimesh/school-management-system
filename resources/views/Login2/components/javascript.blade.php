4483d1e89e7a84deeaf3de664735b2979f88e9a2
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-05-17 22:05:06 +0530

    New Login
    New Login Done !!

---
+++ resources/views/Login2/components/javascript.blade.php
@@ -0,0 +1,19 @@
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="/js/animation.js"></script>
+
+<!-- Eye js -->
+<script>
+    $(".toggle-password").click(function () {
+
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+</script>
